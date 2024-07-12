e70f5f3aa76f37b73023d3e518a106270070890c
Md.Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2024-07-13 00:33:39 +0600

    working jwt

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
 const app = express();
 const port = process.env.PORT || 5500;
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173']
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1vlp1px.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,102 +23,92 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
-    // Create a db
     const carInfo = client.db('vehicles').collection('cars');
-    const carCollection = client.db('vehicles').collection('bookings')
+    const carCollection = client.db('vehicles').collection('bookings');
 
-    
-    // CODE HERE
+
+    // ===================== AUTH ==========================
+    //AUTH RELATED API
+
+    app.post('/jwt', async (req, res) => {
+        const user = req.body;
+        console.log(user)
+        res.send(user)
+    });
+
+    // ======================================================
+
+
+
     app.get('/cars', async (req, res) => {
       const cursor = carInfo.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.get('/cars/:id', async(req, res)=> {
+    app.get('/cars/:id', async(req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      //it's used be default - i take specific some data
+      const query = { _id: new ObjectId(id) };
       const options = {
-        projection : {
-          _id: 1,
-          title: 1,
-          brand: 1,
-          type: 1,
-          price: 1,
-          location: 1,
-          mileage:1,
-          photoLink: 1
+        projection: {
+          _id: 1, title: 1, brand: 1, type: 1, price: 1, transmission: 1,
+          year: 1, condition: 1, rating: 1, location: 1, mileage: 1, photoLink: 1, description: 1
         }
-      }
-      const result = await carInfo.findOne(query, options)
-      res.send(result)
-    })
+      };
+      const result = await carInfo.findOne(query, options);
+      res.send(result);
+    });
 
-    app.post('/cars', async(req, res)=> {
+    app.post('/cars', async(req, res) => {
       const car = req.body;
-      console.log(car)
+      console.log('CAR data', car)
       const result = await carInfo.insertOne(car);
       res.send(result);
-    })
 
+    });
 
-
-    // ============================ car booking ============================
-
-    //step-1
-    app.post('/bookings', async(req, res)=> {
+  
+    app.post('/bookings', async(req, res) => {
       const booking = req.body;
-      console.log(booking)
       const result = await carCollection.insertOne(booking);
       res.send(result);
-    })
+    });
 
-    
-    //step-2
+
     app.get('/bookings', async(req, res) => {
-      console.log(req.query.brand);
       let query = {};
-      //given only specific query taking data
-      if(req.query.brand){
-        query = {brand: req.query.brand}
+      if (req.query.brand) {
+        query = { brand: req.query.brand };
       }
       const result = await carCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
     app.delete('/bookings/:id', async(req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await carCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
-    // update & just state change - confirm -> confirmed
     app.patch('/bookings/:id', async(req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) };
       const updateBooking = req.body;
-      console.log('booking confirm ', updateBooking);
+      const updateDoc = { $set: { status: updateBooking.status } };
+      const result = await carCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
-      const updateDoc = {$set:{status:updateBooking.status}}
-      const result = await carCollection.updateOne(filter, updateDoc)
-      res.send(result)
-    })
-
-
-
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
+  } catch (error) {
+    console.error(error);
   }
 }
+
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
